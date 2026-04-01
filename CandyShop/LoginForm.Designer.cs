4b68786ce31cb7091b98e131eedd8b05c3d175d8
elisha <timofeyberillo2005@mail.ru>
2026-04-01 17:08:42 +0300

    Добавил форму авторизации LoginForm

--- CandyShop/LoginForm.Designer.cs
+++ CandyShop/LoginForm.Designer.cs
@@ -28,12 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            txtLogin = new TextBox();
+            txtPassword = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(285, 73);
+            label1.Name = "label1";
+            label1.Size = new Size(89, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Вход в систему";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(218, 142);
+            label2.Name = "label2";
+            label2.Size = new Size(41, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Логин";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(218, 189);
+            label3.Name = "label3";
+            label3.Size = new Size(49, 15);
+            label3.TabIndex = 3;
+            label3.Text = "Пароль";
+            // 
+            // txtLogin
+            // 
+            txtLogin.Location = new Point(273, 139);
+            txtLogin.Name = "txtLogin";
+            txtLogin.Size = new Size(192, 23);
+            txtLogin.TabIndex = 4;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(273, 186);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(192, 23);
+            txtPassword.TabIndex = 5;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(671, 380);
+            button1.Name = "button1";
+            button1.Size = new Size(80, 28);
+            button1.TabIndex = 6;
+            button1.Text = "Войти";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // LoginForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(txtPassword);
+            Controls.Add(txtLogin);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "LoginForm";
+            Text = "Авторизация";
+            Load += LoginForm_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txtLogin;
+        private TextBox txtPassword;
+        private Button button1;
     }
 }
--- CandyShop/LoginForm.cs
+++ CandyShop/LoginForm.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Windows.Forms;
+
 namespace CandyShop
 {
     public partial class LoginForm : Form
@@ -5,6 +8,55 @@ namespace CandyShop
         public LoginForm()
         {
             InitializeComponent();
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+            txtPassword.UseSystemPasswordChar = true;
+
+            this.Text = "Авторизация";
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.AcceptButton = button1;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string login = txtLogin.Text.Trim();
+            string password = txtPassword.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(login) || string.IsNullOrWhiteSpace(password))
+            {
+                MessageBox.Show(
+                    "Введите логин и пароль.",
+                    "Ошибка",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (login == "admin" && password == "12345")
+            {
+                MessageBox.Show(
+                    "Вход выполнен успешно!",
+                    "Успех",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+
+                MainForm mainForm = new MainForm();
+                mainForm.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show(
+                    "Неверный логин или пароль.",
+                    "Ошибка входа",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+
+                txtPassword.Clear();
+                txtPassword.Focus();
+            }
         }
     }
 }
