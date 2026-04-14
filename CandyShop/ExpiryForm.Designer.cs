3fba676ee33b707a2ff53f7d6c6df0a90bb49b8c
elisha <timofeyberillo2005@mail.ru>
2026-04-14 19:52:02 +0300

    Добавил форму контроля сроков годности

--- CandyShop/ExpiryForm.Designer.cs
+++ CandyShop/ExpiryForm.Designer.cs
@@ -28,12 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "ExpiryForm";
+            label1 = new Label();
+            numDays = new NumericUpDown();
+            btnLoad = new Button();
+            dgvExpiry = new DataGridView();
+            colProduct = new DataGridViewTextBoxColumn();
+            colQuantity = new DataGridViewTextBoxColumn();
+            colExpiryDate = new DataGridViewTextBoxColumn();
+            colDaysLeft = new DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)numDays).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dgvExpiry).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(49, 55);
+            label1.Name = "label1";
+            label1.Size = new Size(144, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Срок годности до (дней):";
+            // 
+            // numDays
+            // 
+            numDays.Location = new Point(211, 53);
+            numDays.Name = "numDays";
+            numDays.Size = new Size(499, 23);
+            numDays.TabIndex = 1;
+            // 
+            // btnLoad
+            // 
+            btnLoad.Location = new Point(286, 91);
+            btnLoad.Name = "btnLoad";
+            btnLoad.Size = new Size(186, 52);
+            btnLoad.TabIndex = 2;
+            btnLoad.Text = "Показать";
+            btnLoad.UseVisualStyleBackColor = true;
+            btnLoad.Click += btnLoad_Click;
+            // 
+            // dgvExpiry
+            // 
+            dgvExpiry.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvExpiry.Columns.AddRange(new DataGridViewColumn[] { colProduct, colQuantity, colExpiryDate, colDaysLeft });
+            dgvExpiry.Location = new Point(31, 158);
+            dgvExpiry.Name = "dgvExpiry";
+            dgvExpiry.Size = new Size(755, 277);
+            dgvExpiry.TabIndex = 3;
+            // 
+            // colProduct
+            // 
+            colProduct.HeaderText = "Товар";
+            colProduct.Name = "colProduct";
+            // 
+            // colQuantity
+            // 
+            colQuantity.HeaderText = "Количество";
+            colQuantity.Name = "colQuantity";
+            // 
+            // colExpiryDate
+            // 
+            colExpiryDate.HeaderText = "Срок годности";
+            colExpiryDate.Name = "colExpiryDate";
+            // 
+            // colDaysLeft
+            // 
+            colDaysLeft.HeaderText = "Осталось дней";
+            colDaysLeft.Name = "colDaysLeft";
+            // 
+            // ExpiryForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dgvExpiry);
+            Controls.Add(btnLoad);
+            Controls.Add(numDays);
+            Controls.Add(label1);
+            Name = "ExpiryForm";
+            Text = "ExpiryForm";
+            Load += ExpiryForm_Load;
+            ((System.ComponentModel.ISupportInitialize)numDays).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dgvExpiry).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private NumericUpDown numDays;
+        private Button btnLoad;
+        private DataGridView dgvExpiry;
+        private DataGridViewTextBoxColumn colProduct;
+        private DataGridViewTextBoxColumn colQuantity;
+        private DataGridViewTextBoxColumn colExpiryDate;
+        private DataGridViewTextBoxColumn colDaysLeft;
     }
 }
--- CandyShop/ExpiryForm.cs
+++ CandyShop/ExpiryForm.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
 
 namespace CandyShop
 {
@@ -15,6 +9,67 @@ namespace CandyShop
         public ExpiryForm()
         {
             InitializeComponent();
+        }
+
+        private void ExpiryForm_Load(object sender, EventArgs e)
+        {
+            this.Text = "Сроки годности";
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+            dgvExpiry.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvExpiry.RowHeadersVisible = false;
+            dgvExpiry.ReadOnly = true;
+            dgvExpiry.AllowUserToAddRows = false;
+
+            numDays.Value = 7;
+
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            dgvExpiry.Rows.Clear();
+
+            int days = (int)numDays.Value;
+
+            using (var connection = DatabaseHelper.GetConnection())
+            {
+                connection.Open();
+
+                string query = @"
+                    SELECT 
+                        p.Name,
+                        w.Quantity,
+                        w.ExpiryDate,
+                        DATEDIFF(day, GETDATE(), w.ExpiryDate) AS DaysLeft
+                    FROM Warehouse w
+                    JOIN Products p ON w.ProductId = p.Id
+                    WHERE w.ExpiryDate <= DATEADD(day, @days, GETDATE())
+                    ORDER BY w.ExpiryDate";
+
+                using (var cmd = new SqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("@days", days);
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            dgvExpiry.Rows.Add(
+                                reader["Name"],
+                                reader["Quantity"],
+                                Convert.ToDateTime(reader["ExpiryDate"]).ToShortDateString(),
+                                reader["DaysLeft"]
+                            );
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            LoadData();
         }
     }
 }
